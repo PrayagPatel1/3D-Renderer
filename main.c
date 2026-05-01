40a9ab4b48ef2ae3c25b33bb9e5be18d6a6e2bde
Prayag Patel <prayagpatel283@gmail.com>
2026-05-01 17:21:24 -0400

    Modified the build script to link the Win32 api system libraries for windowing and modified the main.c file to create and show a blank window

--- main.c
+++ main.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
 
-int main()
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance,
+                   HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine,
+                   int nCmdShow)
 {
-    printf("Hello World \n");
+    // === Define a Window Class ===
+    const wchar_t CLASS_NAME[] = L"Renderer Window";
+
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = NULL;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    // === Window Creation ===
+    HWND window_handle = CreateWindowEx(0,
+                                        CLASS_NAME,
+                                        L"3D Renderer",
+                                        WS_OVERLAPPEDWINDOW,
+                                        CW_USEDEFAULT,
+                                        CW_USEDEFAULT,
+                                        CW_USEDEFAULT,
+                                        CW_USEDEFAULT,
+                                        NULL,      // parent window
+                                        NULL,      // Menu
+                                        hInstance, // Instance handle
+                                        NULL       // additional application data
+    );
+
+    if (!window_handle)
+    {
+        return 0;
+    }
+
+    ShowWindow(window_handle, nCmdShow);
+
     return 0;
 }
