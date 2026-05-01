819f077e1befa8be46aadeec063c84a3590b3af7
Prayag Patel <prayagpatel283@gmail.com>
2026-05-01 15:41:28 -0400

    Modified the build.sh script and the main.c file

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-SOURCE_FILE = "main.c"
-OUTPUT_EXE = "3d-renderer"
+SOURCE_FILE="main.c"
+OUTPUT_EXE="3d-renderer"
 
 gcc -Wall -Wextra "$SOURCE_FILE" -o "$OUTPUT_EXE"
 
-if [$? -eq 0]; then
-    echo "Build Successful. Running $OUTPUT_EXE"
+if [ $? -eq 0 ]; then
+    echo "Build Successful. Running $OUTPUT_EXE..."
     ./"$OUTPUT_EXE"
 else
-    echo "Build Failed"
+    echo "Build Failed."
     exit 1
 fi 
